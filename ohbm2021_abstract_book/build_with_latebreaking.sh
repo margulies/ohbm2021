f7bd353b9d2741a0b4b833b03313f4999cfc05e8
Hao-Ting Wang <htwangtw@gmail.com>
2021-06-09 17:20:08 +0100

    Update output

--- ohbm2021_abstract_book/build_with_latebreaking.sh
+++ ohbm2021_abstract_book/build_with_latebreaking.sh
@@ -1,26 +1,14 @@
 python get_latebreaking_data.py
 
-# Linux
-# sed -i -e 's/<u>/\\underline\{/g' 2021_abstracts.csv
-# sed -i -e 's/<\/u>/\}/g' 2021_abstracts.csv
-# sed -i -e 's/<sup>/\\textsuperscript\{/g' 2021_abstracts.csv
-# sed -i -e 's/<\/sup>/\}/g' 2021_abstracts.csv
-# sed -i -e 's/\ \&\ /\ \\\&\ /g' 2021_abstracts.csv
-# sed -i -e 's/\&E/\\\&E/g' 2021_abstracts.csv
-# sed -i -e 's/A\&/A\\\&/g' 2021_abstracts.csv
-# sed -i -e 's/\_/\\\_/g' 2021_abstracts.csv
-# sed -i -e 's/\\n\\n/\\linebreak/g' 2021_abstracts.csv
-
-# Mac
-# !sed -i -e 's/<u>/\\underline\{/g' 2021_abstracts.csv
-# !sed -i -e 's/<\/u>/\}/g' 2021_abstracts.csv
-# !sed -i -e 's/<sup>/\\textsuperscript\{/g' 2021_abstracts.csv
-# !sed -i -e 's/<\/sup>/\}/g' 2021_abstracts.csv
-# !sed -i -e 's/\ \&\ /\ \\\&\ /g' 2021_abstracts.csv
-# !sed -i -e 's/\&E/\\\&E/g' 2021_abstracts.csv
-# !sed -i -e 's/A\&/A\\\&/g' 2021_abstracts.csv
-# !sed -i -e 's/\_/\\\_/g' 2021_abstracts.csv
-# !sed -i -e 's/\\n\\n/\\linebreak/g' 2021_abstracts.csv
+sed -i -e 's/<u>/\\underline\{/g' 2021_abstracts.csv
+sed -i -e 's/<\/u>/\}/g' 2021_abstracts.csv
+sed -i -e 's/<sup>/\\textsuperscript\{/g' 2021_abstracts.csv
+sed -i -e 's/<\/sup>/\}/g' 2021_abstracts.csv
+sed -i -e 's/\ \&\ /\ \\\&\ /g' 2021_abstracts.csv
+sed -i -e 's/\&E/\\\&E/g' 2021_abstracts.csv
+sed -i -e 's/A\&/A\\\&/g' 2021_abstracts.csv
+sed -i -e 's/\_/\\\_/g' 2021_abstracts.csv
+sed -i -e 's/\\n\\n/\\linebreak/g' 2021_abstracts.csv
 
 # remove first line
 for FILE in 2021_authors_index.csv 2021_categories_index.csv ; do
