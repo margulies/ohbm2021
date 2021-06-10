c174cb64552a44ce2d2e4ad88da1f6e40ac89852
Hao-Ting Wang <htwangtw@gmail.com>
2021-06-10 18:56:54 +0100

    save all fields on the web; add presenter field

--- ohbm2021_abstract_book/poster_info.py
+++ ohbm2021_abstract_book/poster_info.py
@@ -1,7 +1,14 @@
 """Parse poster info to a json
 """
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import json
 import re
+from collections import Counter
+from string import punctuation as PUNCTUATION
+
+
 from parser_web import main as parser
 import utils as ut
 import pandas as pd
@@ -16,9 +23,9 @@ def get_categories(categories):
     return [pc, sc]
 
 
-def get_author_names(speakers):
-    """Get author names"""
-    authors = re.findall(r'\<[\/sup]*\>[, ]*([A-Za-z -.üÁ]*)\<[\/sup]*\>', speakers)
+def get_author_names(authors):
+    """Parse the 'author' field"""
+    authors = re.findall(r'\<[\/sup]*\>[, ]*([\w -.]*)\<[\/sup]*\>', authors)
     return [auth for auth in authors if auth != '']
 
 
@@ -35,15 +42,55 @@ def find_pdf(row, media_links):
     return media_links.loc[mask, ['PDF Link', 'Thumbnail Link']].values.tolist()[0]
 
 
+def clean_middlename(middlename):
+    if isinstance(middlename, str):
+        return "".join(re.findall("[A-Za-z ]+", middlename))
+    else:
+        return None
+
+
+def clean_first_lastname(name):
+    return ut._capitalise_name(name)
+
+
+def get_author_info(speakers):
+    all_speakers = {}
+    for speak in speakers['speaker']:
+        if speak['middlename']:
+            name = [clean_first_lastname(speak['firstname']),
+                    # clean_middlename(speak['middlename']),
+                    clean_first_lastname(speak['lastname'])]
+        else:
+            name = [clean_first_lastname(speak['firstname']),
+                    clean_first_lastname(speak['lastname'])]
+
+        current_speaker = {
+            'name': ' '.join(name),
+            'affiliation':speak['company'],
+            'presentingAuthor': speak['@role'] == 'AbsAuthor',
+        }
+        all_speakers[f"speaker{speak['@id']}"] = current_speaker
+    return all_speakers
+
+
 data = parser()
 media_links = pd.read_csv("data/ohbm-ALL-poster-links.csv")
+with open("abstract.json", "r") as f:
+    abstract_keywords = json.load(f)
+
 posters = {}
 for idx, row in data.iterrows():
     pdf, thumbnail = find_pdf(row, media_links)
+    speakers_details = get_author_info(row['speakers'])
+    author_names = []
+    for id in speakers_details:
+        if speakers_details[id]['presentingAuthor']:
+            presenter = speakers_details[id]['name']
+        author_names.append(speakers_details[id]['name'])
     current_poster = {
         'title': row['title'],
-        'authorName': ",".join(get_author_names(row['authors'])),
-        'authors': get_author_names(row['authors']),
+        'authorName': ",".join(author_names),
+        'authors': author_names,
         'introduction': row['purpose'],
         'methods': row['materialsMethods'],
         'results': row['results'],
@@ -52,9 +99,15 @@ for idx, row in data.iterrows():
         'thumbnailUrl': thumbnail,
         'category': get_categories(row["categories"]["category"]),
         'previewurl': row['previewurl'],
+        'presenter': presenter
         }
+
+    # for abstr in abstract_keywords:
+    #     if abstr['number'] == int(row['abstractNumber']):
+    #         current_poster['softwareDemo'] = abstr['software-demo']
+    #         # current_poster['keywords'] = abstr['keywords'] + abstr['abstract']
+
     posters[f"poster{row['abstractNumber']}"] = current_poster
 
-print(posters["poster1051"]['methods'])
 with open("posters.json", 'w', encoding='utf8') as file:
     json.dump(posters, file, indent=2, ensure_ascii=False)
