bd6e3c559fd9c28cb3bd40161577bf02223737ab
margulies <daniel.margulies@gmail.com>
2021-06-13 21:33:17 +0200

    updated for exhibit hall venues

--- sparkle_upload/scripts/create-posterpages.ts
+++ sparkle_upload/scripts/create-posterpages.ts
@@ -22,14 +22,16 @@ if (!projectId || !credentialPath || !filePath) {
 interface posterpage {
   name: string;
   title: string;
-  authorName: string;
-  iframeUrl: string;
-  introVideoUrl: string;
-  thumbnailUrl: string;
-  category: string;
-  // text: string;
-  // abstractURL: string;
-  // authorUserId: string;
+  authorName?: string;
+  iframeUrl?: string;
+  introVideoUrl?: string;
+  thumbnailUrl?: string;
+  category?: string;
+  contactEmail?: string;
+  moreInfoUrl?: string;
+  parentId: string;
+  chatTitle: string;
+  moreInfoUrlTitle?: string;
 }
 
 // const venueId = name.replace(/\W/g, "").toLowerCase();
@@ -43,51 +45,55 @@ initFirebaseAdminApp(projectId, {
 fs.createReadStream(filePath)
   .pipe(csv())
   .on("data", async (poster: posterpage) => {
-      await admin
-        .firestore()
-        .collection("venues")
-        .doc(poster.name)
-        .update({
-          template: "posterpage",
-          chatTitle: "Poster",
-          attendeesTitle: "Attendees",
-          parentId: "posterhall",
-          name: poster.name,
-          iframeUrl: poster.iframeUrl,
-          config: {
-            landingPageConfig: {
-              coverImageUrl: "",
-              subtitle: "",
-              presentation: [],
-              checkList: [],
-            },
+    const categories = poster.category ? poster.category.split(',') : "";
+    await admin
+      .firestore()
+      .collection("venues")
+      .doc(poster.name)
+      .set({
+        template: "posterpage",
+        chatTitle: poster.chatTitle,
+        attendeesTitle: "Attendees",
+        parentId: poster.parentId,
+        name: poster.title,
+        iframeUrl: poster.iframeUrl,
+        config: {
+          landingPageConfig: {
+            coverImageUrl: "",
+            subtitle: "",
+            presentation: [],
+            checkList: [],
           },
-          host: {
-            icon: "",
-          },
-          poster: {
-            authorName: poster.authorName,
-            introVideoUrl: poster.introVideoUrl,
-            thumbnailUrl: poster.thumbnailUrl,
-            title: poster.title,
-            categories: poster.category.split(','),
-            //text: poster.text,
-            //abstractURL: poster.abstractURL,
-            //authorUserId: poster.authorUserId,
-          },
-          owners: [], // NEED TO ADD PRESENTER ID
-          isLive: false,
-          code_of_conduct_questions: [],
-          profile_questions: [],
-          theme: {
-            primaryColor: "",
-          }
-          //termsAndConditions: TermOfService[] = [],
-          // height,
-          // width,
-        })
-        .then()
-        // add in for situation where posterpage doesn't yet exist
+        },
+        host: {
+          icon: "",
+        },
+        poster: {
+          authorName: poster.authorName,
+          introVideoUrl: poster.introVideoUrl,
+          thumbnailUrl: poster.thumbnailUrl,
+          title: poster.title,
+          categories: categories,
+          //text: poster.text,
+          //authorUserId: poster.authorUserId,
+          //abstractURL: poster.abstractURL,
+          contactEmail: poster.contactEmail,
+          moreInfoUrl: poster.moreInfoUrl,
+          moreInfoUrlTitle: poster.moreInfoUrlTitle,
+        },
+        owners: ["MmLlloenTiO9BRxui8tA4YsTgS13","QYMwGyzmj1UYctLDBEgPf7ZdAhF3","nRW5gaGOIsP9OpWz6xUr9rH13be2"], // NEED TO ADD PRESENTER ID
+        isLive: false,
+        code_of_conduct_questions: [],
+        profile_questions: [],
+        theme: {
+          primaryColor: "#bc271a",
+        }
+        //termsAndConditions: TermOfService[] = [],
+        // height,
+        // width,
+      })
+      .then()
+      // add in for situation where posterpage doesn't yet exist
   })
   .on("end", () => {
     console.log("posters successfully updated")
